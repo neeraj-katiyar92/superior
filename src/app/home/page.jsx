946f08d28ee0246d6e161350839c9ff7d22b2742
Rajesh-dev3 <rajesh@bellatorgroup.in>
2024-08-30 17:42:05 +0530

    testimonial

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -3,30 +3,95 @@ import React, { useState } from 'react'
 import Layout from '../common/Layout'
 import './style.scss'
 import Filter from '../component/filter'
+import { logo1, logo2, logo3, logo4, logo5, testimonialImage } from '../../../public/images'
+import Image from 'next/image'
+import { MdOutlineStarPurple500 } from "react-icons/md";
+import { FcGoogle } from "react-icons/fc";
+import { IoStarHalfSharp } from "react-icons/io5";
+import { FaArrowRight } from "react-icons/fa";
+
+
+
 
 
 const HomePage = () => {
- 
+  const logoImages = [logo1, logo2, logo3, logo4, logo5]
   return (
     <>
-  
-    <Layout>
-      <section className="banner">
-        <div className="banner-container">
-          <h2 className='banner-title'>The Portal Behind Thriving <br />Real Estate Properties.</h2>
-          <div className="filter">
-            <Filter />
+
+      <Layout>
+        <section className="banner">
+          <div className="banner-container">
+            <h2 className='banner-title'>The Portal Behind Thriving <br />Real Estate Properties.</h2>
+            <div className="filter">
+              <Filter />
+            </div>
           </div>
-        </div>        
-      </section>
-      <section className='hot-selling'>
+        </section>
+        <section className='hot-selling'>
+          <div className="title">
+            Hot selling Properties in gurugram
+          </div>
+          <div className="superior-container">
+            <div className="row">
 
-      </section>
+              <ul>
+                {logoImages?.map((list) => <li><Image src={list} alt={list} /></li>)
+                }
+              </ul>
+            </div>
+          </div>
+        </section>
 
- 
-    </Layout>
+        <div className="testimonial">
+          <div className="superior-container">
+            <div className="col">
+
+              <div className="slag">Testimonial</div>
+              <div className="title">
+                <h3>
+                  What our clients think about
+                  Superior Services
+                </h3>
+              </div>
+              <div className="review">
+                <div className="google-icon">
+
+                  <FcGoogle /> <span>
+                    4.5
+                  </span>
+                </div>
+                <div className="star-icon">
+
+                  <MdOutlineStarPurple500 />
+                  <MdOutlineStarPurple500 />
+                  <MdOutlineStarPurple500 />
+                  <MdOutlineStarPurple500 />
+                  <IoStarHalfSharp />
+                  <span>566 reviews</span>
+                </div>
+              </div>
+              <div className="images-grid">
+                <div className="left">
+                  <div className="tag">Right <FaArrowRight/></div>
+                  <div className="visit-superior">Visit superior - better business</div>
+                  <div className="para">How Just Duo Saved 10 Hrs and Reinvested into Business Growth.</div>
+                  <div className="img">
+                    <Image src={testimonialImage}/>
+                  </div>
+                </div>
+                <div className="center"></div>
+                <div className="right"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </Layout>
     </>
   )
 }
 
 export default HomePage
+
+
