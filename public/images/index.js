3389b3d822908b5bb11522a42c31278326a36349
Rajesh-dev3 <rajesh@bellatorgroup.in>
2024-08-30 17:42:05 +0530

    testimonial

---
+++ public/images/index.js
@@ -0,0 +1,7 @@
+import logo1 from "./logo/logo1.png"
+import logo2 from "./logo/logo2.png"
+import logo3 from "./logo/logo3.png"
+import logo4 from "./logo/logo4.png"
+import logo5 from "./logo/logo5.png"
+import testimonialImage from "./testimonial/img.jpg"
+export {logo1,logo2,logo3,logo4,logo5,testimonialImage}
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import Layout from '../common/Layout';
 import Head from 'next/head';
@@ -10,16 +11,38 @@ import Sellingproperties from '../component/sellingproperties';
 
 
 const HomePage = () => {
+=======
+"use client"
+import React, { useState } from 'react'
+import Layout from '../common/Layout'
+import './style.scss'
+import Filter from '../component/filter'
+import { logo1, logo2, logo3, logo4, logo5, testimonialImage } from '../../../public/images'
+import Image from 'next/image'
+import { MdOutlineStarPurple500 } from "react-icons/md";
+import { FcGoogle } from "react-icons/fc";
+import { IoStarHalfSharp } from "react-icons/io5";
+import { FaArrowRight } from "react-icons/fa";
+
+
+
+
+
+const HomePage = () => {
+  const logoImages = [logo1, logo2, logo3, logo4, logo5]
+>>>>>>> 946f08d (testimonial)
   return (
     <>
-  
-    <Layout>
-      <section className="banner">
-        <div className="banner-container">
-          <h2 className='banner-title'>The Portal Behind Thriving <br />Real Estate Properties.</h2>
-          <div className="filter">
-            <Filter />
+
+      <Layout>
+        <section className="banner">
+          <div className="banner-container">
+            <h2 className='banner-title'>The Portal Behind Thriving <br />Real Estate Properties.</h2>
+            <div className="filter">
+              <Filter />
+            </div>
           </div>
+<<<<<<< HEAD
         </div>        
       </section>
 
@@ -53,8 +76,73 @@ const HomePage = () => {
 
 
     </Layout>
+=======
+        </section>
+        <section className='hot-selling'>
+          <div className="title">
+            Hot selling Properties in gurugram
+          </div>
+          <div className="superior-container">
+            <div className="row">
+
+              <ul>
+                {logoImages?.map((list) => <li><Image src={list} alt={list} /></li>)
+                }
+              </ul>
+            </div>
+          </div>
+        </section>
+
+        <div className="testimonial">
+          <div className="superior-container">
+            <div className="col">
+
+              <div className="slag">Testimonial</div>
+              <div className="title">
+                <h3>
+                  What our clients think about
+                  Superior Services
+                </h3>
+              </div>
+              <div className="review">
+                <div className="google-icon">
+
+                  <FcGoogle /> <span>
+                    4.5
+                  </span>
+                </div>
+                <div className="star-icon">
+
+                  <MdOutlineStarPurple500 />
+                  <MdOutlineStarPurple500 />
+                  <MdOutlineStarPurple500 />
+                  <MdOutlineStarPurple500 />
+                  <IoStarHalfSharp />
+                  <span>566 reviews</span>
+                </div>
+              </div>
+              <div className="images-grid">
+                <div className="left">
+                  <div className="tag">Right <FaArrowRight/></div>
+                  <div className="visit-superior">Visit superior - better business</div>
+                  <div className="para">How Just Duo Saved 10 Hrs and Reinvested into Business Growth.</div>
+                  <div className="img">
+                    <Image src={testimonialImage}/>
+                  </div>
+                </div>
+                <div className="center"></div>
+                <div className="right"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </Layout>
+>>>>>>> 946f08d (testimonial)
     </>
   )
 }
 
 export default HomePage
+
+
